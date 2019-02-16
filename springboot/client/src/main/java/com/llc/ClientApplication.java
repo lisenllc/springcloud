ba320ab39f6164987fa626772ce960dc9ea6926c
Liangcheng Li <llc3202073@163.com>
2019-02-16 18:39:27 +0800

    Update ClientApplication.java

--- springboot/client/src/main/java/com/llc/ClientApplication.java
+++ springboot/client/src/main/java/com/llc/ClientApplication.java
@@ -1,22 +1,21 @@
 package com.llc;
 
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
-@EnableEurekaClient
 @SpringBootApplication(scanBasePackages = "com.llc")
 public class ClientApplication {
-  public static void main(String[] args) {
-    new SpringApplicationBuilder(ClientApplication.class).web(true).run(args);
-  }
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(ClientApplication.class).web(true).run(args);
+    }
 
-  @Bean
-  @LoadBalanced
-  RestTemplate restTemplate() {
-    return new RestTemplate();
-  }
+    @Bean
+    @LoadBalanced
+    RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 }
