ea6cdaa0f3a25d258a3fa6d4e5ed92cb63dca776
Liangcheng Li <llc3202073@163.com>
2019-02-16 18:39:08 +0800

    Update HelloController.java

--- springboot/client/src/main/java/com/llc/controller/HelloController.java
+++ springboot/client/src/main/java/com/llc/controller/HelloController.java
@@ -1,6 +1,5 @@
 package com.llc.controller;
 
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,18 +9,25 @@ import org.springframework.web.client.RestTemplate;
 @RestController
 public class HelloController {
 
-  @Autowired RestTemplate restTemplate;
+    @Autowired
+    RestTemplate restTemplate;
 
-  @Value("${name}")
-  String foo;
+    //  @Value("${neo.hello}")
+    @Value("${neo.hello}")
+    private String hello;
 
-  @RequestMapping("/")
-  @HystrixCommand(fallbackMethod = "hiError")
-  public String home() {
-    return restTemplate.getForObject("http://SERVER/", String.class);
-  }
+    @RequestMapping("/hello")
+    public String from() {
+        return this.hello;
+    }
 
-  public String hiError() {
-    return foo+"hi,sorry,error!";
-  }
+//  @RequestMapping("/")
+//  @HystrixCommand(fallbackMethod = "hiError")
+//  public String home() {
+//    return restTemplate.getForObject("http://SERVER/", String.class);
+//  }
+
+    public String hiError() {
+        return "hi,sorry,error!";
+    }
 }
